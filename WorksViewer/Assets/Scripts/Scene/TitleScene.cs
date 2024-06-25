cd09803e462e1234d87f7d08bdbd1205afff60b2
Yeo <yeodaehyeon@gmail.com>
2024-06-25 16:09:21 +0900

    인게임 UI 작업 진행중 임시 커밋

--- WorksViewer/Assets/Scripts/Scene/TitleScene.cs
+++ WorksViewer/Assets/Scripts/Scene/TitleScene.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
-using System;
-using System.Threading;
+using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
+    Button _titleButton;
     public void StartScene()
     {
-
+        DontDestroyOnLoad(gameObject);
+        _titleButton = transform.GetComponentInChildren<Button>();
+        _titleButton.onClick.AddListener(()=> AsyncSceneChange().Forget());
+    }
+    private async UniTask AsyncSceneChange()
+    {
+        try
+        {
+            await UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("GameScene").ToUniTask();
+            Debug.Log("Scene changed successfully");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Failed to load scene: {e.Message}");
+        }
+        finally
+        {
+            //GameObject.Find("GameScene").GetComponent<GameScene>().StartScene();
+            DestroyImmediate(gameObject);
+        }
     }
 }
